fb77bc5504b2c51e61e16c3655aaebbc572c9334
David Aguilar <davvid@gmail.com>
2024-02-14 18:34:19 -0800

    garden 1.2.1
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- Formula/garden.rb
+++ Formula/garden.rb
@@ -1,8 +1,8 @@
 class Garden < Formula
   desc "Grow and cultivate collections of Git trees"
   homepage "https://gitlab.com/garden-rs/garden"
-  url "https://gitlab.com/garden-rs/garden/-/archive/v1.2.0/garden-v1.2.0.tar.gz"
-  sha256 "a8e72c8fa57c427eb04d0fcafc10d88e7958b611a582bd91c87e1cf829947455"
+  url "https://gitlab.com/garden-rs/garden/-/archive/v1.2.0/garden-v1.2.1.tar.gz"
+  sha256 "facccb43e90dac24882a1b2799b71f3abdba3906ff8e72ba72e850f7b3148477"
   license "MIT"
   head "https://gitlab.com/garden-rs/garden.git", branch: "main"
 
@@ -18,13 +18,14 @@ class Garden < Formula
         current:
           path: ${GARDEN_CONFIG_DIR}
           commands:
-            test: |
-              touch ${TREE_NAME}
-              test: touch ${name}
-        name: $ echo garden
+            test: touch ${TREE_NAME}
+      commands:
+        test: touch ${filename}
+      variables:
+        filename: $ echo output
     EOS
     system bin/"garden", "-vv", "test", "current"
-    assert_predicate testpath/"garden", :exist?
     assert_predicate testpath/"current", :exist?
+    assert_predicate testpath/"output", :exist?
   end
 end

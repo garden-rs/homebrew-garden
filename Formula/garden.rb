ab1fe9d29f03d7cdb06cb22349573e62303739d9
David Aguilar <davvid@gmail.com>
2024-03-21 03:13:32 -0700

    garden 1.4.0
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- Formula/garden.rb
+++ Formula/garden.rb
@@ -1,8 +1,8 @@
 class Garden < Formula
   desc "Grow and cultivate collections of Git trees"
   homepage "https://gitlab.com/garden-rs/garden"
-  url "https://gitlab.com/garden-rs/garden/-/archive/v1.3.0/garden-v1.3.0.tar.gz"
-  sha256 "56f28b92bc891f5bb5fc3bb857e90d81373274db4001608191004886a005b070"
+  url "https://gitlab.com/garden-rs/garden/-/archive/v1.4.0/garden-v1.4.0.tar.gz"
+  sha256 "1bf3a2d6dc6bdfd3d69918bc142f79a5f6d0e8643536b54dd3bc965b24f88d96"
   license "MIT"
   head "https://gitlab.com/garden-rs/garden.git", branch: "main"
 
